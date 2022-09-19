4343638f23ba5b36c9e3bc8ba4b8cac6dae79f3d
kumar <kumar@DESKTOP-7B09MNR>
2022-09-19 21:43:26 +0530

    First Commit-Engineer 1

---
+++ src/test/java/practice/ContactTest.java
@@ -0,0 +1,11 @@
+package practice;
+
+import org.testng.annotations.Test;
+
+public class ContactTest {
+	@Test
+	public void contact() {
+		System.out.println("this is my first commit-maven project");
+	}
+
+}
